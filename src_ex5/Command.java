8386c75db225cc67e6dea98e241902630a1d3047
Klunnyk Dmytro <117164181+CTTAPKU@users.noreply.github.com>
2024-03-08 01:04:37 +0200

    Delete src_ex5 directory

--- src_ex5/Command.java
+++
@@ -1,9 +0,0 @@
-package src_ex5;
-
-/**
- *
- * @author Klunnyk Dmytro
- */
-public interface Command {
-    public void execute();
-}
--- src_ex5/ConsoleCommand.java
+++
@@ -1,9 +0,0 @@
-package src_ex5;
-
-/**
- *
- * @author Klunnyk Dmytro
- */
-public interface ConsoleCommand extends Command {
-        public int getKey();
-}
