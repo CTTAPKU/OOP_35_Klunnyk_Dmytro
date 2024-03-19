351af7a3e43ea0376b1347f2596025f9ee5571ad
Klunnyk Dmytro <117164181+CTTAPKU@users.noreply.github.com>
2024-03-19 11:18:49 +0200

    Add files via upload

---
+++ Task5/src/main/src_ex5/UndoCommand.java
@@ -0,0 +1,32 @@
+package src_ex5;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import src_ex3.item;
+import src_ex3.View;
+
+/**
+ *
+ * @author cttar
+ */
+public class UndoCommand {
+
+    private static final String UNAME = "undo.bin";
+
+    private ArrayList<item> items;
+
+    private View view;
+
+    public UndoCommand(View view) {
+        this.view = view;
+    }
+
+    public void Undo() throws IOException, Exception{
+        ObjectInputStream is = new ObjectInputStream(new FileInputStream(UNAME));
+        items = (ArrayList<item>) is.readObject();
+        is.close();
+    }
+
+}
---
+++ Task5/src/main/src_ex5/UndoConsoleCommand.java
@@ -0,0 +1,38 @@
+package src_ex5;
+
+import src_ex3.View;
+
+/**
+ *
+ * @author cttar
+ */
+public class UndoConsoleCommand implements ConsoleCommand {
+
+    private View view;
+
+    public UndoConsoleCommand(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public int getKey() {
+        return 6;
+    }
+
+    @Override
+    public String toString() {
+        return "6. Undo\n";
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Undo current.");
+        try {
+            UndoCommand und = new UndoCommand(view);
+            und.Undo();
+        } catch (Exception e) {
+            System.err.println("Serialization error: " + e);
+        }
+        view.viewShow();
+    }
+}
